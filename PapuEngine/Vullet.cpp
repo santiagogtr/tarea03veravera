0938095d42d8fb32390244fce12a3138e69cd33c
Rancés <rances.ramos.r@gmail.com>
2017-12-06 00:11:55 -0500

    Cambio en la bala

--- PapuEngine/Vullet.cpp
+++ PapuEngine/Vullet.cpp
@@ -13,8 +13,14 @@ void Vullet::draw(SpriteBacth& spritebatch) {
 	const glm::vec4 uvRect(0.0f, 0.0f, 1.0f, 1.0f);
 	ColorRGBA  color;
 	color.set(255, 255, 0, 255);
-	glm::vec4 destRect(_position.x, _position.y, 20, 35);
-	spritebatch.draw(destRect, uvRect, _textureID, 0.0f, color);
+	if (facing) {
+		glm::vec4 destRect(_position.x + 50, _position.y + 35, -25, 15);
+		spritebatch.draw(destRect, uvRect, _textureID, 0.0f, color);
+	}
+	else {
+		glm::vec4 destRect(_position.x - 70, _position.y + 35, 25, 15);
+		spritebatch.draw(destRect, uvRect, _textureID, 0.0f, color);
+	}
 }
 
 bool Vullet::update(float deltaTime, int pantalla) {
